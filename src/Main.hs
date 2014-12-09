af4a924f49a7cdad565832412ab605861bbc5eaa
Jesse Trimble <jesseltrimble@gmail.com>
2014-12-09 09:05:28 -0700

    Initial simple implementation

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,38 @@
 module Main where
+  import System.Environment  (getArgs)
+  import Data.Time.LocalTime (getZonedTime)
+  import Data.Time.Format    (formatTime, defaultTimeLocale)
+  import Data.List           (intercalate)
 
-  main = putStrLn "Stuff!"
+  main = do
+    args <- getArgs
+    processArgs args
+
+  processArgs [] = printNotes
+  processArgs args = do
+    addition <- formatNoteLine $ getIncomingNote args
+    appendFile notesFilePath addition
+
+  printNotes = do
+    notes <- readFile notesFilePath
+    putStr $ unlines $ reverse $ take 10 $ reverse $ lines notes
+
+  currentFormattedTime = do
+    t <- getZonedTime
+    return (formatTime defaultTimeLocale "%D %r" t)
+
+  formatNoteLine s = do
+    formattedTime <- currentFormattedTime
+    return $ formattedTime ++ " - " ++ s ++ "\n"
+
+  getIncomingNote []   = ""
+  getIncomingNote [x]  = x
+  getIncomingNote args = let
+    postDashyArgs = tail $ dropWhile (\x -> x /= "--") args
+    in
+      messageFrom postDashyArgs
+      where
+        messageFrom []    = head args
+        messageFrom args' = intercalate " " args'
+
+  notesFilePath = "./littlenotes.txt"
